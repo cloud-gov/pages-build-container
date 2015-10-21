4a55cefec8e5922f2a90345c4c0223bbb4bc3b6a
Dave Cole <david.cole@gsa.gov>
2015-10-21 13:20:21 -0400

    Support multiple generators for build (hugo is a stub)

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO cancel build task if it exceeds timeout
+
 # Stop script on errors
 set -e
 set -o pipefail
@@ -15,10 +17,37 @@ unset GITHUB_TOKEN
 if [[ "$GENERATOR" = "jekyll" && -f Gemfile ]]; then
 
   # Add Federalist configuration settings
-  echo "\nbaseurl: ${BASEURL-"''"}\nbranch: ${BRANCH}\n${CONFIG}" >> _config.yml
+  echo -e "\nbaseurl: ${BASEURL-"''"}\nbranch: ${BRANCH}\n${CONFIG}" >> _config.yml
 
-  # Install dependencies
-  bundle install
+  # Run the build process from the jekyll docker image
+  strip_jekyll() {
+    if grep -qE "gem\s+(?:\"|')jekyll(?:\"|')" Gemfile; then
+      sed -ri "/^gem\s+(\"|')jekyll\1.*$/d" \
+        Gemfile
+    fi
+  }
+
+  if [ "$FORCE_APK_INSTALL" ]; then
+    install_user_pkgs_from_file
+  fi
+
+  if [ -f "Gemfile" ] || [ "$1" = "bundle" ]; then
+    if [ "$UPDATE_GEMFILE" ]; then
+      chpst -u jekyll:jekyll docker-helper backup_gemfile
+      chpst -u jekyll:jekyll docker-helper copy_default_gems_to_gemfile
+      chpst -u jekyll:jekyll docker-helper make_gemfile_uniq
+
+      strip_jekyll
+      chpst -u jekyll:jekyll docker-helper \
+        add_gemfile_dependency "$JEKYLL_VERSION"
+    fi
+
+    chpst -u jekyll:jekyll docker-helper install_users_gems
+    if ! grep -qE "^\s*gem\s+(\"|')jekyll(\"|')" Gemfile; then
+      echo "Moving Gemfile to Gemfile.docker because jekyll is not included."
+      mv /srv/jekyll/Gemfile /srv/jekyll/Gemfile.docker
+    fi
+  fi
 
   # Run Jekyll as the jekyll user, pinned to directories
   chpst -u jekyll:jekyll bundle exec jekyll build --source . --destination ./_site
@@ -28,7 +57,7 @@ if [[ "$GENERATOR" = "jekyll" && -f Gemfile ]]; then
 elif [ "$GENERATOR" = "jekyll" ]; then
 
   # Add Federalist configuration settings
-  echo "\nbaseurl: ${BASEURL-"''"}\nbranch: ${BRANCH}\n${CONFIG}" >> _config.yml
+  echo -e "\nbaseurl: ${BASEURL-"''"}\nbranch: ${BRANCH}\n${CONFIG}" >> _config.yml
 
   # Run Jekyll as the jekyll user, pinned to directories
   chpst -u jekyll:jekyll jekyll build --source . --destination ./_site
@@ -36,7 +65,8 @@ elif [ "$GENERATOR" = "jekyll" ]; then
 
 # Hugo
 elif [ "$GENERATOR" = "hugo" ]; then
-  hugo -b ${BASEURL-"''"} -s . -d ./_site
+  echo "Hugo not installed!"
+  # hugo -b ${BASEURL-"''"} -s . -d ./_site
 
 # Static files
 else
