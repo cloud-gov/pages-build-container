57d2de16f6655f238427ea0367e8be3e7bea084c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-10-25 14:08:41 -0500

    Change AWS region to GovCloud region
    This commit changes the region that `publish.sh` publishes sites to from E/W to GovCloud.
    This was necessary because we are migrating our sites from S3 in E/W to S3 in GovCloud.

--- publish.sh
+++ publish.sh
@@ -16,7 +16,7 @@ for i in `find . | grep -E "\.html$|\.css$|\.js$|\.json$|\.svg$"`; do
   fi
 done
 
-export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-gov-west-1
 
 # sync compressed files
 aws s3 sync . s3://${BUCKET}/${PREFIX}/ --no-follow-symlinks \
