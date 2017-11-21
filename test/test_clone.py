045801b6d0b647bfcec88105edd2ec600f0c3eb5
James Seppi <james.seppi@gsa.gov>
2017-11-21 12:32:13 -0600

    implement smoketests for all sub-tasks

--- test/test_clone.py
+++ test/test_clone.py
@@ -2,13 +2,24 @@ import os
 
 from invoke import MockContext, Result
 
-from tasks import clone_repo
-
-# See http://docs.pyinvoke.org/en/latest/concepts/testing.html
+from tasks import clone_repo, push_repo_remote
 
 
-def test_clone():
-    # TODO
-    os.environ['GITHUB_TOKEN'] = 'fake_token'
-    ctx = MockContext(run=Result("TODO\n"))
-    clone_repo(ctx, owner='owner', repository='repo', branch='master')
+class TestCloneRepo():
+    def test_it_is_callable(self):
+        os.environ['GITHUB_TOKEN'] = 'fake_token'
+        ctx = MockContext(run=Result('git clone result'))
+        clone_repo(ctx, owner='owner', repository='repo', branch='master')
+
+
+class TestPushRepoRemote():
+    def test_it_is_callable(self):
+        os.environ['GITHUB_TOKEN'] = 'fake_token'
+
+        ctx = MockContext(run=[
+            Result('git remote add result'),
+            Result('git push result')
+            ]
+        )
+        push_repo_remote(ctx, owner='owner', repository='repo',
+                         branch='branch', remote_name='boop')
