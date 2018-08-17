38b9d2ba8b76a3fb3e1958da1098ffa1ddf2f108
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-08-17 16:51:13 -0400

    change generator from javascript to build only

--- tasks/main.py
+++ tasks/main.py
@@ -273,7 +273,7 @@ def main(ctx):
                 run_task(ctx, 'build-static',
                          log_callback=LOG_CALLBACK,
                          private_values=private_values)
-            elif GENERATOR == 'javascript':
+            elif GENERATOR == 'script only':
                 LOGGER.info('build already ran in \'npm run federalist\'')
             else:
                 raise ValueError(f'Invalid GENERATOR: {GENERATOR}')
