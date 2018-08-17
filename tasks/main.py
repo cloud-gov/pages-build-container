fba36b1149eb34b8e6ff38357b8f8ae0b55b28db
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-08-17 15:58:54 -0400

    javascript build init

--- tasks/main.py
+++ tasks/main.py
@@ -273,6 +273,8 @@ def main(ctx):
                 run_task(ctx, 'build-static',
                          log_callback=LOG_CALLBACK,
                          private_values=private_values)
+            elif GENERATOR == 'javascript':
+                LOGGER.info('site build already ran using \'npm run federalist\'')
             else:
                 raise ValueError(f'Invalid GENERATOR: {GENERATOR}')
 
