d3fef7d8eccdf16e7246085c7a676b42d18f323b
Will Slack <wslack@users.noreply.github.com>
2018-08-21 19:30:15 -0400

    typo fixes

--- publishing/s3publisher.py
+++ publishing/s3publisher.py
@@ -97,7 +97,7 @@ def publish_to_s3(directory, base_url, site_prefix, bucket, cache_control,
         msg_404.append(" the <a href='/'>homepage?<a/>.</p><br>")
         msg_404.append(" <p>This site is hosted by")
         msg_404.append(" <a href='https://federalist.18f.gov'>Federalist</a>.")
-        msg_404.append("</h3></body></html>")
+        msg_404.append("</p></body></html>")
         msg_404 = ''.join(msg_404)
         makedirs(path.dirname(filename_404), exist_ok=True)
         with open(filename_404, "w+") as f:
