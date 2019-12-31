6a69faa1a22a78e3b049dc11ea2f5fcdf3771b7a
David Corwin <david.corwin@gsa.gov>
2019-12-31 15:38:45 -0800

    Move migrate to method call

--- bin/migrate.py
+++ bin/migrate.py
@@ -2,12 +2,17 @@ import os
 import psycopg2
 
 
-conn = psycopg2.connect(os.environ['DB_URL'])
-cursor = conn.cursor()
-cursor.execute(
-    'CREATE TABLE IF NOT EXISTS buildlog (id serial PRIMARY KEY, '
-    'build integer, source varchar, output varchar)'
-)
-conn.commit()
-cursor.close()
-conn.close()
+def migrate():
+    conn = psycopg2.connect(os.environ['DB_URL'])
+    cursor = conn.cursor()
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS buildlog (id serial PRIMARY KEY, '
+        'build integer, source varchar, output varchar)'
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    migrate()
