68a9f83e2f569bc53dee414c41bcc4aa6c62041c
David Corwin <david.corwin@gsa.gov>
2020-03-25 13:42:41 -0700

    Configuration updates

--- bin/push-docker-image.sh
+++ bin/push-docker-image.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
+# federalist-garden-build-dev-task
+TAG=$1
+
 # Make sure local registry is running on localhost:5000
-docker build --tag federalist-garden-build-dev .
-docker tag federalist-garden-build-dev localhost:5000/federalist-garden-build-dev
-docker push localhost:5000/federalist-garden-build-dev
+docker build --tag $TAG .
+docker tag $TAG localhost:5000/$TAG
+docker push localhost:5000/$TAG
