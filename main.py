8bd897a36d6c3bf7adb0daff30c4cf7882d2a113
David Corwin <david.corwin@gsa.gov>
2020-05-07 18:57:07 -0700

    Update manifests to use tasks only in non prod, ensure uev is loaded from env when using tasks

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ import sys
 from dotenv import load_dotenv
 from tasks import main
 
+def load_vcap():
+    if os.getenv('VCAP_APPLICATION', '') == '':
+        return
+
+    vcap_application = json.loads(os.getenv('VCAP_APPLICATION', '{}'))
+    vcap_services = json.loads(os.getenv('VCAP_SERVICES', '{}'))
+
+    space = vcap_application['space_name']
+
+    uev_ups = next(
+        ups for ups in vcap_services['user-provided']
+        if ups['name'] == f'federalist-{space}-uev-key'
+    )
+
+    uev_env_var = 'USER_ENVIRONMENT_VARIABLE_KEY'
+    os.environ[uev_env_var] = uev_ups['credentials']['key']
+
 
 def load_env():
     '''
@@ -20,19 +37,6 @@ def load_env():
     if os.path.exists(DOTENV_PATH):
         print('Loading environment from .env file')
         load_dotenv(DOTENV_PATH)
-    else:
-        vcap_application = json.loads(os.getenv('VCAP_APPLICATION', '{}'))
-        vcap_services = json.loads(os.getenv('VCAP_SERVICES', '{}'))
-
-        space = vcap_application['space_name']
-
-        uev_ups = next(
-            ups for ups in vcap_services['user-provided']
-            if ups['name'] == f'federalist-{space}-uev-key'
-        )
-
-        uev_env_var = 'USER_ENVIRONMENT_VARIABLE_KEY'
-        os.environ[uev_env_var] = uev_ups['credentials']['key']
 
 
 if __name__ == "__main__":
@@ -45,8 +49,9 @@ if __name__ == "__main__":
         params = json.loads(args.params)
         for k, v in params.items():
             os.environ[k] = v
-
     else:
         load_env()
+
+    load_vcap()
 
     main()
