9dbc66aa0b438464c6ad0c1b0679336d664f1847
David Corwin <david.corwin@gsa.gov>
2020-06-05 10:30:03 -0700

    Keep rolling strategy just in case

--- .cloudgov/deploy.sh
+++ .cloudgov/deploy.sh
@@ -14,6 +14,6 @@ echo "Logging in to $CF_ORGANIZATION org, $CF_SPACE space."
 cf7 login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
 echo "Deploying to $CF_SPACE space."
-cf7 push $CF_APP --vars-file $CF_VARS_FILE -f $CF_MANIFEST
+cf7 push $CF_APP --strategy rolling --vars-file $CF_VARS_FILE -f $CF_MANIFEST
 
 cf7 logout
