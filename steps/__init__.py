4bcdec1f5395c549ccf78e3d57befcd3405583e2
David Corwin <david.corwin@gsa.gov>
2020-06-25 09:13:04 -0700

    Refactor node steps, update tests

--- steps/__init__.py
+++ steps/__init__.py
@@ -1,5 +1,11 @@
-from .build import build_static
+from .build import build_static, run_federalist_script, setup_node
 from .fetch import fetch_repo
 from .publish import publish
 
-__all__ = ['build_static', 'fetch_repo', 'publish']
+__all__ = [
+    'build_static',
+    'fetch_repo',
+    'publish',
+    'run_federalist_script',
+    'setup_node',
+]
