ed534cbe105565c9386001496f878d68d8c60620
David Corwin <david.corwin@gsa.gov>
2020-06-25 13:48:47 -0700

    Add additional tests

--- test/test_runner.py
+++ test/test_runner.py
@@ -1,8 +1,9 @@
+from pytest import raises
 import shlex
 import subprocess  # nosec
 from unittest.mock import Mock, patch
 
-from runner import run
+from runner import run, run_with_node, NVM_SH_PATH
 
 
 @patch('subprocess.Popen', autospec=True)
@@ -62,7 +63,34 @@ def test_run_popen_failure(mock_popen):
 
 
 @patch('subprocess.Popen', autospec=True)
-def test_run_command_failure(mock_popen):
+def test_run_popen_failure_check_true(mock_popen):
+    mock_logger = Mock()
+    command = 'foobar'
+
+    mock_popen.side_effect = ValueError('ugh')
+
+    with raises(ValueError, match='ugh'):
+        run(mock_logger, command, check=True)
+
+    mock_popen.assert_called_once_with(
+        shlex.split(command),
+        cwd=None,
+        env=None,
+        shell=False,
+        executable=None,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE,
+        bufsize=1,
+        encoding='utf-8',
+        text=True,
+    )
+
+    mock_logger.error.assert_any_call('Encountered a problem invoking Popen.')
+    mock_logger.error.assert_any_call('ugh')
+
+
+@patch('subprocess.Popen', autospec=True)
+def test_run_os_failure(mock_popen):
     mock_logger = Mock()
     command = 'foobar'
 
@@ -89,3 +117,105 @@ def test_run_command_failure(mock_popen):
     mock_logger.error.assert_any_call('ugh')
 
     assert result == 1
+
+
+@patch('subprocess.Popen', autospec=True)
+def test_run_os_failure_check_true(mock_popen):
+    mock_logger = Mock()
+    command = 'foobar'
+
+    mock_popen.side_effect = OSError('ugh')
+
+    with raises(OSError, match='ugh'):
+        run(mock_logger, command, check=True)
+
+    mock_popen.assert_called_once_with(
+        shlex.split(command),
+        cwd=None,
+        env=None,
+        shell=False,
+        executable=None,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE,
+        bufsize=1,
+        encoding='utf-8',
+        text=True
+    )
+
+    mock_logger.error.assert_any_call(
+        'Encountered a problem executing `' + ' '.join(shlex.split(command)) + '`.'
+    )
+    mock_logger.error.assert_any_call('ugh')
+
+
+@patch('subprocess.Popen', autospec=True)
+def test_run_command_failure(mock_popen):
+    mock_logger = Mock()
+    command = 'foobar'
+    return_code = 2
+
+    mock_popen.return_value.__enter__.return_value.returncode = return_code
+
+    result = run(mock_logger, command)
+
+    mock_popen.assert_called_once_with(
+        shlex.split(command),
+        cwd=None,
+        env=None,
+        shell=False,
+        executable=None,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE,
+        bufsize=1,
+        encoding='utf-8',
+        text=True
+    )
+
+    assert result == return_code
+
+
+@patch('subprocess.Popen', autospec=True)
+def test_run_command_failure_check_true(mock_popen):
+    mock_logger = Mock()
+    command = 'foobar'
+    return_code = 2
+
+    mock_popen.return_value.__enter__.return_value.returncode = return_code
+
+    with raises(subprocess.CalledProcessError):
+        run(mock_logger, command, check=True)
+
+    mock_popen.assert_called_once_with(
+        shlex.split(command),
+        cwd=None,
+        env=None,
+        shell=False,
+        executable=None,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE,
+        bufsize=1,
+        encoding='utf-8',
+        text=True
+    )
+
+
+@patch('runner.run', autospec=True)
+def test_run_with_node(mock_run):
+    mock_logger = Mock()
+    command = 'foobar'
+    cwd = '/foo'
+    env = {}
+    check = True
+
+    result = run_with_node(mock_logger, command, cwd=cwd, env=env, check=check)
+
+    assert result == mock_run.return_value
+
+    mock_run.assert_called_once_with(
+        mock_logger,
+        f'source {NVM_SH_PATH} && {command}',
+        cwd=cwd,
+        env=env,
+        shell=True,  # nosec
+        check=check,
+    )
