418c6e2170f4d055d09d283178f2fce1f54815d0
Drew Bollinger <drew.bollinger@gsa.gov>
2023-01-20 15:21:25 -0500

    source rvm for gemdir lookup

--- src/steps/build.py
+++ src/steps/build.py
@@ -368,7 +368,12 @@ def setup_bundler(should_cache: bool, bucket, s3_client):
     cache_folder = None
     if GEMFILELOCK_PATH.is_file() and should_cache:
         logger.info(f'{GEMFILELOCK} found. Attempting to download cache')
-        GEMFOLDER = subprocess.run(["rvm", "gemdir"], capture_output=True)  # nosec
+        GEMFOLDER = subprocess.run(
+            f'source {RVM_PATH} && rvm gemdir',
+            shell=True,
+            executable='/bin/bash',
+            capture_output=True,
+        )  # nosec
         GEMFOLDER = GEMFOLDER.stdout.decode('utf-8').strip()
         cache_folder = CacheFolder(GEMFILELOCK_PATH, GEMFOLDER, bucket, s3_client, logger)
         cache_folder.download_unzip()
