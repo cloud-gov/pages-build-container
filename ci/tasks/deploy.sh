a138ed74057f6358829624de47d1e2b26b884ffe
Drew Bollinger <drew.bollinger@gsa.gov>
2023-03-21 14:19:54 -0400

    small fixes to hardening deploy

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -10,5 +10,5 @@ cf t -o $CF_ORG -s $CF_SPACE
 CF_DOCKER_PASSWORD=${AWS_SECRET_ACCESS_KEY} cf push $CF_APP_NAME \
   -f $CF_MANIFEST \
   --vars-file $CF_VARS_FILE \
-  --docker-image "${IMAGE_REPOSITORY}@$(cat ${IMAGE_DIGEST})" \
+  --docker-image "${IMAGE_REPOSITORY}:${IMAGE_TAG}" \
   --docker-username ${AWS_ACCESS_KEY_ID}
