6c8740abf254b53871fe723ee6e39dc18702ac4b
Drew Bollinger <drew.bollinger@gsa.gov>
2023-03-21 12:20:05 -0400

    add oci build + registry image put for secret passing

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -7,8 +7,8 @@ cf auth
 
 cf t -o $CF_ORG -s $CF_SPACE
 
-cf push $CF_APP_NAME \
+CF_DOCKER_PASSWORD=${AWS_SECRET_ACCESS_KEY} cf push $CF_APP_NAME \
   -f $CF_MANIFEST \
   --vars-file $CF_VARS_FILE \
-  --docker-image "${CF_DOCKER_IMAGE_REPOSITORY}@$(cat ${CF_DOCKER_IMAGE_DIGEST})" \
-  --docker-username $CF_DOCKER_USERNAME
+  --docker-image "${IMAGE_REPOSITORY}@$(cat ${IMAGE_DIGEST})" \
+  --docker-username ${AWS_ACCESS_KEY_ID}
